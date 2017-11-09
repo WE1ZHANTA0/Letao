f16a6e107656759bb665a325bc3c12dd7117342c
Git账号 <wzt9349426826>
2017-11-09 10:32:03 +0800

    每个分页共同部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -8,3 +8,48 @@ $(document).ajaxStop(function(){
         NProgress.done();
     }, 500);
 })
+
+
+
+if(location.href.indexOf('login.html') == -1){
+    $.ajax({
+        type:'get',
+        url:'/employee/checkRootLogin',
+        success:function(data){
+            if(data.error == 400){
+                location.href = 'login.html';
+            }
+        }
+    })
+}
+
+
+$('.child').prev().on('click',function(){
+    $(this).next().slideToggle();
+});
+
+$('.btn_menu').on('click',function(){
+    $('.lt_aside').toggleClass('now');
+    $('.lt_main').toggleClass('now');
+});
+
+
+
+
+$('.btn_logout').on('click',function(){
+    $('#logoutModal').modal("show");
+
+
+    $('.btn_confirm').off().on('click',function(){
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            success:function(data){
+                if(data.success){
+                    location.href = 'login.html';
+                }
+            }
+        })
+    })
+})
+
